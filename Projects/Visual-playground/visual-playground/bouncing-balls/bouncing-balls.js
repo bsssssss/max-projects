dc44611a36152a6b1a7572d883e76756ba410466
Bss <mx.bss@proton.me>
2025-05-12 00:39:47 +0200

    good spot to check boundaries bug

--- Projects/Visual-playground/visual-playground/bouncing-balls/bouncing-balls.js
+++ Projects/Visual-playground/visual-playground/bouncing-balls/bouncing-balls.js
@@ -8,8 +8,12 @@ class Ball
     this.radius   = radius;
     this.color    = color;
 
-    this.speed_x = 6;
-    this.speed_y = 1;
+    this.direction_x = 1;
+    this.direction_y = 1;
+    this.speed_x     = 0;
+    this.speed_y     = 21;
+    this.velocity_x;
+    this.velocity_y;
   }
 
   draw()
@@ -20,7 +24,37 @@ class Ball
 
   update()
   {
-    this.position.x += 1 * this.speed_x;
+    this.check_collisions();
+    this.velocity_x  = this.speed_x * this.direction_x;
+    this.velocity_y  = this.speed_y * this.direction_y;
+    this.position.x += this.velocity_x;
+    this.position.y += this.velocity_y;
+  }
+
+  check_collisions()
+  {
+    // TODO: Calculate speed adjustment to reach boundaries on next frame
+
+    let nxt_left_bound   = (this.position.x - this.radius) + this.velocity_x;
+    let nxt_right_bound  = (this.position.x + this.radius) + this.velocity_x;
+    let nxt_lower_bound  = (this.position.y + this.radius) + this.velocity_y;
+    let nxt_higher_bound = (this.position.y - this.radius) + this.velocity_y;
+
+    // post(`[${lower_bound}/${height}]\n`);
+    // post(`[${nxt_higher_bound}/${height}]\n`);
+
+    if (nxt_left_bound < 0) {
+      this.direction_x = 1;
+    }
+    if (nxt_right_bound > width) {
+      this.direction_x = -1;
+    }
+    if (nxt_higher_bound < 0) {
+      this.direction_y = 1;
+    }
+    if (nxt_lower_bound > height) {
+      this.direction_y = -1;
+    }
   }
 
   show()
@@ -36,8 +70,8 @@ class Ball
 const m = mgraphics;
 
 const task    = new Task(run);
-task.interval = 1000 / 30;
-task.repeat();
+task.interval = 1000 / 2;
+task.repeat(-1, 10);
 
 m.init();
 m.relative_coords = 0;
@@ -67,18 +101,17 @@ function init()
 
 function paint()
 {
-  width  = m.size[0];
-  height = m.size[1];
   background();
 
   for (ball of balls)
   {
-    ball.draw();
+    ball.show();
   }
 }
 
 function run()
 {
+  ball.update();
   m.redraw();
 }
 
@@ -87,6 +120,12 @@ function background()
   m.set_source_rgba(0.2, 0.2, 0.2, 1);
   m.rectangle(0, 0, width, height);
   m.fill();
+}
+
+function onresize(w, h)
+{
+  width  = w;
+  height = h;
 }
 
 function deg_to_rad(v)
