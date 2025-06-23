8b81084740986f66192d6b4a4d4886ad9224b252
Bss <mx.bss@proton.me>
2025-06-23 15:15:48 +0200

    remove hello world message

--- Projects/web-ui/web-ui/patchers/server.js
+++ Projects/web-ui/web-ui/patchers/server.js
@@ -1,8 +1,6 @@
 const maxAPI = require("max-api");
 const http = require("node:http");
 
-maxAPI.post("Hello from node !!!");
-
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain"});
     res.end('Oh un serveur !');
