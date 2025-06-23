a891e953766b9bfb3ceafe9089894f9d910f3c1d
Bss <mx.bss@proton.me>
2025-06-23 15:15:11 +0200

    basic server setup

--- Projects/web-ui/web-ui/patchers/server.js
+++ Projects/web-ui/web-ui/patchers/server.js
@@ -1,3 +1,13 @@
 const maxAPI = require("max-api");
+const http = require("node:http");
 
 maxAPI.post("Hello from node !!!");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain"});
+    res.end('Oh un serveur !');
+});
+
+server.listen(7400, () => {
+    maxAPI.post("Started server at http://localhost:7400");
+})
