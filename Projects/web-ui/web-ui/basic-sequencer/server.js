799481ab184c818c107cf1752bccc051e1572cb5
Bss <mx.bss@proton.me>
2025-06-24 00:20:04 +0200

    add websocket server

--- Projects/web-ui/web-ui/basic-sequencer/server.js
+++ Projects/web-ui/web-ui/basic-sequencer/server.js
@@ -3,26 +3,23 @@ const http = require('node:http');
 const fs = require('fs');
 const ws = require('ws');
 
-const wss = new ws.WebSocketServer({port: 7401});
+const wss = new ws.WebSocketServer({ port: 7401 });
 let clients = [];
 
 wss.on('connection', (socket) => {
-    maxAPI.post("WebSocket client connected");
-    clients.push(socket);
+  maxAPI.post('WebSocket client connected');
+  clients.push(socket);
 
-    socket.on('close', () => {
-        clients = clients.filter(client => client !== socket);
-        maxAPI.post('WebSocket client disconnected');
-    })
-})
+  socket.on('close', () => {
+    clients = clients.filter((client) => client !== socket);
+    maxAPI.post('WebSocket client disconnected');
+  });
+});
 
-const htmlPath = "./public/index.html";
-fs.watch(htmlPath, (eventType, filename) => {
-    maxAPI.post(`${filename} => ${eventType}`);
-})
-
+const htmlPath = './public/index.html';
 const server = http.createServer((req, res) => {
   fs.readFile(htmlPath, (err, html) => {
+    maxAPI.post(html.toString());
     res.writeHead(200, {
       'Content-Type': 'text/html',
       'Content-length': html.length,
@@ -34,4 +31,8 @@ const server = http.createServer((req, res) => {
 
 server.listen(7400, () => {
   maxAPI.post('Started server at http://localhost:7400');
+});
+
+fs.watch(htmlPath, (eventType, filename) => {
+  maxAPI.post(`${filename} => ${eventType}`);
 });
