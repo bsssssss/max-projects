1723b83ef280271b2949c817b45a93c3e87086b1
Bss <mx.bss@proton.me>
2025-06-24 00:28:54 +0200

    formatting

--- Projects/web-ui/web-ui/basic-sequencer/server.js
+++ Projects/web-ui/web-ui/basic-sequencer/server.js
@@ -7,32 +7,32 @@ const wss = new ws.WebSocketServer({ port: 7401 });
 let clients = [];
 
 wss.on('connection', (socket) => {
-  maxAPI.post('WebSocket client connected');
-  clients.push(socket);
+    maxAPI.post('WebSocket client connected');
+    clients.push(socket);
 
-  socket.on('close', () => {
-    clients = clients.filter((client) => client !== socket);
-    maxAPI.post('WebSocket client disconnected');
-  });
+    socket.on('close', () => {
+        clients = clients.filter((client) => client !== socket);
+        maxAPI.post('WebSocket client disconnected');
+    });
 });
 
 const htmlPath = './public/index.html';
+fs.watch(htmlPath, (eventType, filename) => {
+    maxAPI.post(`${filename} => ${eventType}`);
+});
+
 const server = http.createServer((req, res) => {
-  fs.readFile(htmlPath, (err, html) => {
-    maxAPI.post(html.toString());
-    res.writeHead(200, {
-      'Content-Type': 'text/html',
-      'Content-length': html.length,
+    fs.readFile(htmlPath, (err, html) => {
+        maxAPI.post(html.toString());
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-length': html.length,
+        });
+        res.write(html);
+        res.end();
     });
-    res.write(html);
-    res.end();
-  });
 });
 
 server.listen(7400, () => {
-  maxAPI.post('Started server at http://localhost:7400');
-});
-
-fs.watch(htmlPath, (eventType, filename) => {
-  maxAPI.post(`${filename} => ${eventType}`);
+    maxAPI.post('Started server at http://localhost:7400');
 });
