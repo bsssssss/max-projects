7d0a4570fe4ad650aa6a95ce5c9af7ab9cfd389b
Bss <mx.bss@proton.me>
2025-06-30 00:03:38 +0200

    reorganize

--- Projects/mini-notation/patchers/mini-notation-parser.js
+++ Projects/mini-notation/patchers/mini-notation-parser.js
@@ -2,26 +2,23 @@ function parse(...expr) {
     let output = [];
 
     for (let token of expr) {
-        if (typeof token == 'string' && token.includes('!')) {
+        if (typeof token == 'number') {
+            output.push(token);
+        } else if (typeof token == 'string' && token.includes('!')) {
             let elem = token.split('!');
             for (i = 0; i < elem[1]; i++) {
                 let res = sanitizeType(elem[0]);
                 output.push(res);
             }
-        } else {
-            output.push(token);
         }
     }
-
     outlet(0, output);
 }
 
 function sanitizeType(elem) {
     if (isFinite(elem)) {
-        post(`found number ${elem}` + '\n');
         return parseFloat(elem);
     } else {
-        post(`found symbol ${elem}` + '\n');
         return elem;
     }
 }
