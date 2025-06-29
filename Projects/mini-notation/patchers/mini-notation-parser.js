f4d75da8a30b3e73d93cc42b079ab10648f50903
Bss <mx.bss@proton.me>
2025-06-29 23:43:54 +0200

    handle number vs symbols events

--- Projects/mini-notation/patchers/mini-notation-parser.js
+++ Projects/mini-notation/patchers/mini-notation-parser.js
@@ -1,10 +1,27 @@
 function parse(...expr) {
     let output = [];
+
     for (let token of expr) {
-        let elem = token.split('!');
-        for (i = 0; i < elem[1]; i++) {
-            output.push(parseFloat(elem[0]));
+        if (typeof token == 'string' && token.includes('!')) {
+            let elem = token.split('!');
+            for (i = 0; i < elem[1]; i++) {
+                let res = sanitizeType(elem[0]);
+                output.push(res);
+            }
+        } else {
+            output.push(token);
         }
     }
+
     outlet(0, output);
+}
+
+function sanitizeType(elem) {
+    if (isFinite(elem)) {
+        post(`found number ${elem}` + '\n');
+        return parseFloat(elem);
+    } else {
+        post(`found symbol ${elem}` + '\n');
+        return elem;
+    }
 }
