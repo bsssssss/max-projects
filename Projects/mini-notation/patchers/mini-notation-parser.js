3ae81319865894d0334d72383ba11f84aef774f8
Bss <mx.bss@proton.me>
2025-06-30 03:05:21 +0200

    maybe simpler

--- Projects/mini-notation/patchers/mini-notation-parser.js
+++ Projects/mini-notation/patchers/mini-notation-parser.js
@@ -2,14 +2,19 @@ function parse(...expr) {
     let output = [];
 
     for (let token of expr) {
-        if (typeof token == 'number') {
+        if (
+            typeof token == 'number' ||
+            (typeof token == 'string' && !token.includes('!'))
+        ) {
             output.push(token);
-        } else if (typeof token == 'string' && token.includes('!')) {
+        } else if (typeof token == 'string') {
             let elem = token.split('!');
             for (i = 0; i < elem[1]; i++) {
                 let res = sanitizeType(elem[0]);
                 output.push(res);
             }
+        } else {
+            post('error: could not process event type');
         }
     }
     outlet(0, output);
