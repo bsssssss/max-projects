a19232943bd461726d8e962ce198f56f09e92fb7
Bss <mx.bss@proton.me>
2025-07-01 17:49:13 +0200

    use max list
    format
    
    update patcher
    
    update notes

--- Projects/mini-notation/patchers/mini-notation-parser.js
+++ Projects/mini-notation/patchers/mini-notation-parser.js
@@ -1,61 +1,52 @@
 outlets = 2;
 
-outlet(1, `test`);
+outlet(1, 'test');
 
-function p(...expr)
-{
+function p(...expr) {
     let parsed = parseExpression(expr);
     post(expr + ' --> ' + parsed + '\n');
 }
 
-function parseExpression(expr)
-{
+function parseExpression(expr) {
     let parsed = [];
     for (let token of expr) {
         if (isAtom(token)) {
             parsed.push(token);
-        }
-        else if (hasOperator(token)) {
+        } else if (hasOperator(token)) {
             let elem = token.split('!');
             for (i = 0; i < elem[1]; i++) {
                 parsed.push(sanitizeType(elem[0]));
             }
-        }
-        else {
-            post('error: could not process expression type');
+        } else {
+            post('error: could not process token type');
         }
     }
     return parsed;
 }
 
-function isAtom(input)
-{
+function isAtom(input) {
     if (
-    typeof input == 'number' ||
-    (typeof input == 'string' && !input.includes('!'))) {
+        typeof input == 'number' ||
+        (typeof input == 'string' && !input.includes('!'))
+    ) {
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
 
-function hasOperator(input)
-{
+function hasOperator(input) {
     if (typeof input == 'string' && input.includes('!')) {
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
 
-function sanitizeType(elem)
-{
+function sanitizeType(elem) {
     if (isFinite(elem)) {
         return parseFloat(elem);
-    }
-    else {
+    } else {
         return elem;
     }
 }
--- Projects/mini-notation/patchers/notes.md
+++ Projects/mini-notation/patchers/notes.md
@@ -2,12 +2,3 @@
 
 Generate complex lists of events with a simple and expressive syntax, inspired
 from TidalCycles/Strudel.
-
-## From Max to JS
-
-The commitment i should make is how to format an expression in Max.
-There are few possibilities :
-
-- a symbol followed by a list
-- a symbol followed by a string
-- a symbol followed by a max array
